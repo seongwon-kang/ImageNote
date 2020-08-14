3fafbb3ba22a6468dd6ebed3f55f06d92207821a
dehehe-mirai <abc7988jpse@gmail.com>
2020-08-14 22:20:45 +0900

    Add Service

--- app/src/main/java/io/hardplant/imagenote/MainActivity.java
+++ app/src/main/java/io/hardplant/imagenote/MainActivity.java
@@ -1,8 +1,15 @@
 package io.hardplant.imagenote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
 import android.os.Bundle;
+
+import io.hardplant.imagenote.service.ClassifierService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createChannelID();
+        runService();
+    }
+
+    private void runService() {
+        Intent intent = new Intent(this, ClassifierService.class);
+
+        startService(intent);
+
+        ClassifierService.enqueueWork(this, intent);
+    }
+
+    private void createChannelID() {
+        CharSequence name = getString(R.string.channel_name);
+        String description = getString(R.string.channel_description);
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(ClassifierService.CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
 }
--- app/src/main/java/io/hardplant/imagenote/service/ClassifierService.java
+++ app/src/main/java/io/hardplant/imagenote/service/ClassifierService.java
@@ -1,14 +1,102 @@
 package io.hardplant.imagenote.service;
 
+import android.app.NotificationManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.os.FileObserver;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.JobIntentService;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import io.hardplant.imagenote.MainActivity;
+import io.hardplant.imagenote.R;
 
 public class ClassifierService extends JobIntentService {
+    public static final String CHANNEL_ID = "notification";
+    private static final String TAG = "ClassifierService";
+    private static final int NOTIFICATION_ID = 39;
+    private static String path = "";
+    private static FileObserver fileObserver;
+
+
+    public static void enqueueWork(Context context, Intent intent) {
+        Log.i(TAG, "enqueueWork: RUNNING");
+
+        //path = intent.getExtras().getString("PATH");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        NotificationCompat.Builder mNotifyBuilder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
+
+        mNotifyBuilder
+                .setOngoing(true)
+                .setContentTitle("ImageNote")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentText("Detect");
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+        notificationManager.notify(NOTIFICATION_ID, mNotifyBuilder.build());
+
+        File dir = new File(path);
+        if (!dir.isDirectory()) {
+            Log.e(TAG, "onCreate: " + dir.getAbsolutePath());
+            //throw new RuntimeException("Not a directory");
+        } else {
+            fileObserver = new FileObserver(dir) {
+                @Override
+                public void onEvent(int i, @Nullable String s) {
+                    switch(i) {
+                        case (FileObserver.CREATE):
+                            moveFile(s);
+                            break;
+                    }
+                }
+            };
+            fileObserver.startWatching();
+        }
+
+    }
+
+    private void moveFile(String s) {
+        File file = new File(s);
+        try {
+            Files.move(Paths.get(file.getAbsolutePath()), getTargetDir(s));
+        } catch (IOException e) {
+
+        }
+    }
+
+    private Path getTargetDir(String s) {
+
+        return Paths.get(new File(s).getAbsolutePath());
+    }
 
     @Override
     protected void onHandleWork(@NonNull Intent intent) {
 
+    }
+
+    @Override
+    public void onDestroy() {
+        fileObserver.stopWatching();
+        fileObserver = null;
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+        notificationManager.cancel(NOTIFICATION_ID);
+
+        super.onDestroy();
     }
 }
