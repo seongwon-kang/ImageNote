6e0425da3844d15c8a93f7afb1c9f83489ec21a8
dehehe-mirai <abc7988jpse@gmail.com>
2020-08-14 18:25:57 +0900

    Add ClassfierSerivce

---
+++ app/src/main/java/io/hardplant/imagenote/service/ClassifierService.java
@@ -0,0 +1,14 @@
+package io.hardplant.imagenote.service;
+
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.JobIntentService;
+
+public class ClassifierService extends JobIntentService {
+
+    @Override
+    protected void onHandleWork(@NonNull Intent intent) {
+
+    }
+}
